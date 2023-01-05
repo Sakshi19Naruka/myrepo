d279786d425280530030a10d68a53ffab1a40477
Sakshi19Naruka <sakshinaruka19@gmail.com>
2023-01-05 10:51:52 +0530

    quicksort added in master

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void bublesort(){
+    //
+}
+void quicksort(){
+    //
+}
+
+int main(){
+cout<<"Hello World"<<endl;
+cout<<"Hello Aayush"<<endl;
+return 0;
+}
